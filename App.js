699ea5e023020044ce6a52f002ec2fb2ced662ff
Ash <eiad_work@yahoo.com>
2020-06-12 19:56:35 +0400

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 import React, { Component } from "react";
 
-// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
-// https://swapi.co/
-// https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
-
 class App extends Component {
   constructor(props) {
     super(props);
